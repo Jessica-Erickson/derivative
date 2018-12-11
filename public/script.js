0f6cde4e7a8dcf8edc4c775d3f59a0d060330a05
Intrepid Panda <jerick999@gmail.com>
2018-12-11 08:57:33 -0700

    Figure out that using sockets probably won't work

--- public/script.js
+++ public/script.js
@@ -1,14 +1,14 @@
+var socket = io();
 var input = document.getElementById('userImage');
 var canvas = document.getElementById('canvas')
 var ctx = canvas.getContext('2d');
-var socket = io();
 
 socket.on('connect', () => {
-
+  console.log('ready');
 });
 
-socket.on('message', (message) => {
-  $('.messages').append(`<h1>'Something came along on the "message" channel: ${message}</h1>`);
+socket.on('received', (received) => {
+  console.log(received);
 });
 
 input.addEventListener('change', setImageToCanvas);
@@ -20,7 +20,7 @@ function setImageToCanvas (event) {
     newImage.addEventListener('load', () => {
       updateCanvasWithImage(newImage);
     });
-    newImage.src = imageURL;
+    newImage.src = imageUrl;
   }
 }
 
@@ -28,156 +28,9 @@ function updateCanvasWithImage (newImage) {
   setCanvasDimensions(newImage);
   ctx.drawImage(newImage,0,0);
   getNewImageData(newImage);
-  createPixelGraph();
-  getStartingPixel();
-  populateBufferPixels();
-  newImage.removeEventListener('load', updateCanvasWithImage(newImage));
-  resetNewImage();
-  resetNewImageData();
-  window.requestAnimationFrame(swapPixels);
-}
-
-function swapPixels () {
-  if (adjacentPixels.length > 0 && bufferPixels.length > 0) {
-    var selected = adjacentPixels[Math.floor(Math.random() * adjacentPixels.length)];
-    var processedAdjacents = pixelGraph[selected].adjacent.filter(function (neighbor) {
-      return sortedPixels.includes(neighbor);
-    });
-    var valueToCompare = processedAdjacents.reduce(function (acc, pixel) {
-      pixelGraph[pixel].rgba.forEach(function (num) {
-        acc += (num / (processedAdjacents.length * 4));
-      });
-      return acc;
-    }, 0);
-
-    var closestValue = '';
-    bufferPixels.forEach(function (pixel) {
-      if (closestValue === '') {
-        closestValue = pixel;
-      }
-      var existingValue = pixelGraph[closestValue].rgba.reduce(function (acc, num) {
-        return acc += (num / 4);
-      },0);
-      var newValue = pixelGraph[pixel].rgba.reduce(function (acc, num) {
-        return acc += (num / 4);
-      },0);
-      if (Math.abs(valueToCompare - existingValue) > Math.abs(valueToCompare - newValue)) {
-        closestValue = pixel;
-      }
-    });
-
-    var bufferColors = pixelGraph[selected].rgba;
-    pixelGraph[selected].rgba = pixelGraph[closestValue].rgba;
-    pixelGraph[closestValue].rgba = bufferColors;
-
-    var selectedCoordinates = selected.split('-');
-    var bufferCoordinates = closestValue.split('-');
-    var cSelected = ctx.createImageData(1, 1);
-    var cBuffer = ctx.createImageData(1, 1);
-
-    cSelected.data[0] = pixelGraph[closestValue].rgba[0];
-    cSelected.data[1] = pixelGraph[closestValue].rgba[1];
-    cSelected.data[2] = pixelGraph[closestValue].rgba[2];
-    cSelected.data[3] = pixelGraph[closestValue].rgba[3];
-    ctx.putImageData(cSelected, bufferCoordinates[0], bufferCoordinates[1]);
-
-    cBuffer.data[0] = pixelGraph[selected].rgba[0];
-    cBuffer.data[1] = pixelGraph[selected].rgba[1];
-    cBuffer.data[2] = pixelGraph[selected].rgba[2];
-    cBuffer.data[3] = pixelGraph[selected].rgba[3];
-    ctx.putImageData(cBuffer, selectedCoordinates[0], selectedCoordinates[1]);
-
-    adjacentPixels = adjacentPixels.filter(function (pixel) {
-      return pixel !== selected;
-    });
-    sortedPixels.push(selected);
-
-    pixelGraph[selected].adjacent.forEach(function (pixel) {
-      if (!sortedPixels.includes(pixel) && !adjacentPixels.includes(pixel)) {
-        adjacentPixels.push(pixel);
-      }
-      if (bufferPixels.includes(pixel)) {
-        bufferPixels = bufferPixels.filter(function (coordinates) {
-          return coordinates !== pixel;
-        });
-        if (unsortedPixels.length > 0) {
-          var newPixel = unsortedPixels[Math.floor(Math.random() * unsortedPixels.length)]
-          bufferPixels.push(newPixel);
-          unsortedPixels = unsortedPixels.filter(function (string) {
-            return string !== newPixel;
-          });
-        }
-      }
-      if (unsortedPixels.includes(pixel)) {
-        unsortedPixels = unsortedPixels.filter(function (coordinates) {
-          return coordinates !== pixel;
-        });
-      }
-    });
-  } else if (adjacentPixels.length > 1) {
-    var selected = adjacentPixels[Math.floor(Math.random() * adjacentPixels.length)];
-    var processedAdjacents = pixelGraph[selected].adjacent.filter(function (neighbor) {
-      return sortedPixels.includes(neighbor);
-    });
-    var valueToCompare = processedAdjacents.reduce(function (acc, pixel) {
-      pixelGraph[pixel].rgba.forEach(function (num) {
-        acc += (num / (processedAdjacents.length * 4));
-      });
-      return acc;
-    }, 0);
-
-    adjacentPixels = adjacentPixels.filter(function (pixel) {
-      return pixel !== selected;
-    });
-    sortedPixels.push(selected);
-
-    var closestValue = '';
-    adjacentPixels.forEach(function (pixel) {
-      if (closestValue === '') {
-        closestValue = pixel;
-      }
-      var existingValue = pixelGraph[closestValue].rgba.reduce(function (acc, num) {
-        return acc += (num / 4);
-      },0);
-      var newValue = pixelGraph[pixel].rgba.reduce(function (acc, num) {
-        return acc += (num / 4);
-      },0);
-      if (Math.abs(valueToCompare - existingValue) > Math.abs(valueToCompare - newValue)) {
-        closestValue = pixel;
-      }
-    });
-
-    var bufferColors = pixelGraph[selected].rgba;
-    pixelGraph[selected].rgba = pixelGraph[closestValue].rgba;
-    pixelGraph[closestValue].rgba = bufferColors;
-
-    var selectedCoordinates = selected.split('-');
-    var bufferCoordinates = closestValue.split('-');
-    var cSelected = ctx.createImageData(1, 1);
-    var cBuffer = ctx.createImageData(1, 1);
-
-    cSelected.data[0] = pixelGraph[closestValue].rgba[0];
-    cSelected.data[1] = pixelGraph[closestValue].rgba[1];
-    cSelected.data[2] = pixelGraph[closestValue].rgba[2];
-    cSelected.data[3] = pixelGraph[closestValue].rgba[3];
-    ctx.putImageData(cSelected, bufferCoordinates[0], bufferCoordinates[1]);
-
-    cBuffer.data[0] = pixelGraph[selected].rgba[0];
-    cBuffer.data[1] = pixelGraph[selected].rgba[1];
-    cBuffer.data[2] = pixelGraph[selected].rgba[2];
-    cBuffer.data[3] = pixelGraph[selected].rgba[3];
-    ctx.putImageData(cBuffer, selectedCoordinates[0], selectedCoordinates[1]);
-
-    pixelGraph[selected].adjacent.forEach(function (pixel) {
-      if (!sortedPixels.includes(pixel) && !adjacentPixels.includes(pixel)) {
-        adjacentPixels.push(pixel);
-      }
-    });
-  } else {
-    console.log('done!');
-    return;
-  }
-  window.requestAnimationFrame(swapPixels);
+  newImage.removeEventListener('load', () => {
+    updateCanvasWithImage(newImage);
+  });
 }
 
 function setCanvasDimensions (newImage) {
@@ -186,74 +39,6 @@ function setCanvasDimensions (newImage) {
 }
 
 function getNewImageData (newImage) {
-  newImageData = ctx.getImageData(0,0,newImage.width,newImage.height);
-}
-
-function createPixelGraph () {
-  var imageData = newImageData.data;
-  var height = newImageData.height;
-  var width = newImageData.width;
-  for (var i = 0; i < imageData.length; i += 4) {
-    var x = ((i / 4) % width);
-    var y = Math.floor((i / 4) / width);
-    var coordinates = x + '-' + y;
-    var top = x + '-' + (y - 1);
-    var right = (x + 1) + '-' + y;
-    var bottom = x + '-' + (y + 1);
-    var left = (x - 1) + '-' + y;
-    pixelGraph[coordinates] = new Pixel(
-      imageData[i], 
-      imageData[i + 1], 
-      imageData[i + 2], 
-      imageData[i + 3]
-    );
-    if ((y - 1) >= 0) {
-      pixelGraph[coordinates].adjacent.push(top);
-    }
-    if ((x + 1) < width) {
-      pixelGraph[coordinates].adjacent.push(right);
-    }
-    if ((y + 1) < height) {
-      pixelGraph[coordinates].adjacent.push(bottom);
-    }
-    if ((x - 1) >= 0) {
-      pixelGraph[coordinates].adjacent.push(left);
-    }
-    unsortedPixels.push(coordinates);
-  }
-}
-
-function resetNewImageData () {
-  newImageData = {};
-}
-
-function Pixel (r, g, b, a) {
-  this.rgba = [r,g,b,a];
-  this.adjacent = [];
-}
-
-function getStartingPixel () {
-  var startingPixel = unsortedPixels[Math.floor(Math.random() * unsortedPixels.length)];
-  sortedPixels.push(startingPixel);
-  unsortedPixels = unsortedPixels.filter(function (coordinate) {
-    return coordinate !== startingPixel;
-  });
-  pixelGraph[startingPixel].adjacent.forEach(function (raddish) {
-    adjacentPixels.push(raddish);
-    unsortedPixels = unsortedPixels.filter(function (turnip) {
-      return turnip !== raddish;
-    });
-  });
-}
-
-function populateBufferPixels () {
-  while (bufferPixels.length < 1000) {
-    var pixel = unsortedPixels[Math.floor(Math.random() * unsortedPixels.length)];
-    unsortedPixels = unsortedPixels.filter(function (coordinate) {
-      return coordinate !== pixel;
-    });
-    if (!sortedPixels.includes(pixel) && !adjacentPixels.includes(pixel)) {
-      bufferPixels.push(pixel);
-    }
-  }
+  var newImageData = ctx.getImageData(0,0,newImage.width,newImage.height);
+  socket.emit('data', newImageData);
 }
--- server.js
+++ server.js
@@ -1,20 +1,58 @@
 const express = require('express');
 const app = express();
-const http = require('http').Server(app);
+const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
 app.locals.name = 'Derivative';
-app.locals.picture = {};
+
+let picture = {};
+let unsortedPixels = [];
 
 io.on('connection', (socket) => {
-  socket.emit('message', `A new user, ${Date.now()}, has connected`);
-
-  socket.on('message', (message) => {
-
-  });
+  const Pixel = (hex) => {
+    this.hex = hex;
+    this.adjacent = [];
+  }
 
   socket.on('disconnect', () => {
 
+  });
+
+  socket.on('data', (data) => {
+    const imageData = data.data;
+    const width = data.width;
+    for (var i = 0; i < imageData.length; i += 4) {
+      const x = ((i / 4) % width);
+      const y = Math.floor((i / 4) / width);
+      const coordinates = x + '-' + y;
+      const newR = '0' + imageData[i].toStrin(16);
+      const newG = '0' + imageData[i + 1].toString(16);
+      const newB = '0' + imageData[i + 2].toString(16);
+      const hexValue = newR.slice(-2) + newG.slice(-2) + newB.slice(-2);
+      const top = x + '-' + (y - 1);
+      const right = (x + 1) + '-' + y;
+      const bottom = x + '-' + (y + 1);
+      const left = (x - 1) + '-' + y;
+
+      picture[coordinates] = new Pixel(hexValue);
+
+      if ((y - 1) >= 0) {
+        picture[coordinates].adjacent.push(top);
+      }
+      if ((x + 1) < width) {
+        picture[coordinates].adjacent.push(right);
+      }
+      if ((y + 1) < height) {
+        picture[coordinates].adjacent.push(bottom);
+      }
+      if ((x - 1) >= 0) {
+        picture[coordinates].adjacent.push(left);
+      }
+
+      unsortedPixels.push(coordinates);
+      console.log(coordinates);
+      // socket.emit('received', coordinates);
+    }
   });
 });
 
@@ -23,6 +61,6 @@ app.use( express.static('public') );
 
 app.set('port', process.env.PORT || 3000);
 
-app.listen(app.get('port'), () => {
+http.listen(app.get('port'), () => {
   console.log(`${app.locals.name} is running on ${app.get('port')}`);
 });
