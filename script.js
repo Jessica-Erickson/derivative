c2b4425d1e77f8b795871cba55a8a20ea4d37291
Intrepid Panda <jerick999@gmail.com>
2019-01-05 21:14:20 -0700

    Set up necessary event listeners and stub their callback function

---
+++ script.js
@@ -0,0 +1,10 @@
+function makeActive(e) {
+  
+}
+
+function setImageToCanvas(e) {
+
+}
+
+document.querySelector('ul').addEventListener('click', makeActive);
+document.querySelector('input').addEventListener('change', setImageToCanvas);
